3038f85dbf0f2c8710205b47ba26c69902c0ed0a
Shoaib Sharif <shoaib19j@gmail.com>
2021-03-11 02:10:13 -0800

    added vendor

---
+++ vendor/sven/artisan-view/src/BlockStack.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Sven\ArtisanView;
+
+use Sven\ArtisanView\Voters\ExtendsParent;
+use Sven\ArtisanView\Voters\SectionsInParent;
+use Sven\ArtisanView\Voters\StacksFromParent;
+use Sven\ArtisanView\Voters\YieldsFromParent;
+use Sven\ArtisanView\Blocks\Block;
+use Symfony\Component\Console\Input\InputInterface;
+
+class BlockStack
+{
+    /**
+     * @var \Sven\ArtisanView\Blocks\Block[]
+     */
+    protected $blocks = [];
+
+    /**
+     * @param \Symfony\Component\Console\Input\InputInterface $input
+     *
+     * @return \Sven\ArtisanView\BlockStack
+     */
+    public function build(InputInterface $input)
+    {
+        $voters = [
+            new ExtendsParent,
+            new YieldsFromParent,
+            new StacksFromParent,
+            new SectionsInParent,
+        ];
+
+        /** @var \Sven\ArtisanView\Voters\Voter $voter */
+        foreach ($voters as $voter) {
+            if (! $voter->canHandle($input)) {
+                continue;
+            }
+
+            $voter->run($input, $this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param \Sven\ArtisanView\Blocks\Block[] ...$blocks
+     *
+     * @return \Sven\ArtisanView\BlockStack
+     */
+    public function add(Block ...$blocks)
+    {
+        foreach ($blocks as $block) {
+            $this->blocks[] = $block;
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return \Sven\ArtisanView\Blocks\Block[]
+     */
+    public function all()
+    {
+        return $this->blocks;
+    }
+}
---
+++ vendor/sven/artisan-view/src/Generator.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Sven\ArtisanView;
+
+class Generator extends ViewActor
+{
+    /**
+     * @param \Sven\ArtisanView\BlockStack $blockStack
+     */
+    public function generate(BlockStack $blockStack)
+    {
+        $views = $this->getViews();
+
+        $this->makeViews(
+            $this->getViewNames($views), $blockStack->all()
+        );
+    }
+
+    /**
+     * @param array                            $names
+     * @param \Sven\ArtisanView\Blocks\Block[] $blocks
+     */
+    protected function makeViews(array $names, array $blocks)
+    {
+        $contents = BlockBuilder::build($blocks);
+
+        foreach ($names as $name) {
+            $path = PathHelper::getPath($name);
+
+            PathHelper::createIntermediateFolders($path);
+
+            file_put_contents($path, $contents);
+        }
+    }
+}
